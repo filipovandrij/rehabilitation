4aa4692c9fea8aa47f6ac2093eabeacc692fc065
Filipov <andrijfilipov73@gmail.com>
2023-02-02 13:43:23 +0200

    start typeset project

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
+import App from './container/App/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-const Title = () => {
-    return <h1>Hi</h1>
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <Title />
+        <App />
     </React.StrictMode>
 )
