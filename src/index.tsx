a55283d1cc3b6127fc7eeff747b806f224f681d6
Filipov <andrijfilipov73@gmail.com>
2023-02-02 13:34:33 +0200

    test how this work

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const Title = () => {
+    return <h1>Hi</h1>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <Title />
+    </React.StrictMode>
+)
