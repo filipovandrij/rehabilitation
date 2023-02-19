e3ff14a662f54ec2c39842e7cdb2601019697d68
Filipov <andrijfilipov73@gmail.com>
2023-02-19 21:59:08 +0200

    add contant to main

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -4,36 +4,38 @@ import Slider from '../../components/Slider/Slider'
 import './Main.scss'
 
 type Props = {}
+
 const Main = (props: Props) => {
     return (
         <StyledEngineProvider injectFirst>
             <main>
                 <Slider></Slider>
+
+                <section className="section_about_clinic">
+                    <div className="container_about">
+                        <h3 className="first">CLINIC</h3>
+                        <p>вислів</p>
+                        <article>
+                            ARTICLE Lorem ipsum dolor, sit amet consectetur
+                            adipisicing elit. Est illum deleniti qui nisi, vel
+                            ea dolore minus assumenda. Odit perferendis
+                            consectetur itaque repellendus rem corrupti laborum
+                            exercitationem provident. Sunt, dolor.
+                        </article>
+                        <p>Мета - яка мета</p>
+                        <Button variant="contained">
+                            Дізнатись трохи більше
+                        </Button>
+                    </div>
+                    <div className="about_video">
+                        <ReactPlayer
+                            className="content_video"
+                            url="https://www.youtube.com/embed/4Sxybl1VaRY"
+                            title="YouTube video player"
+                        ></ReactPlayer>
+                    </div>
+                </section>
                 <Container maxWidth="xl">
-                    <section className="section_about_clinic">
-                        <div className="container_about">
-                            <h3 className="first">CLINIC</h3>
-                            <p>вислів</p>
-                            <article>
-                                Трохи тексту в очі Трохи тексту в очіТрохи
-                                тексту в очі Трохи тексту в очі Трохи тексту в
-                                очі Трохи тексту в очі Трохи тексту в очі Трохи
-                                тексту в очі Трохи тексту в очі{' '}
-                            </article>
-                            <p>Мета - яка мета</p>
-                            <Button variant="contained">
-                                Дізнатись трохи більше
-                            </Button>
-                        </div>
-                        <div className="about_video">
-                            {' '}
-                            <ReactPlayer
-                                className="content_video"
-                                url="https://www.youtube.com/embed/4Sxybl1VaRY"
-                                title="YouTube video player"
-                            ></ReactPlayer>{' '}
-                        </div>
-                    </section>
                     <section className="section_about_services"></section>
                     <section className="section_about_treatment"></section>
                 </Container>
