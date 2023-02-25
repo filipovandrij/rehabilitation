9c28c9cc794b793daa82dab31abdbab43984c7d6
Filipov <andrijfilipov73@gmail.com>
2023-02-25 15:24:13 +0200

    add dropdown menus in app bar

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material'
+import { Button, Link } from '@mui/material'
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration'
 import './Menu.scss'
 
@@ -6,9 +6,102 @@ type Props = {}
 const Menu = (props: Props) => {
     return (
         <>
-            <Button className="btn">About clinic</Button>
-            <Button className="btn">Services</Button>
-            <Button className="btn">Treatment</Button>
+            <Button className="btn">
+                About clinic
+                <div className="dropdown_modal">
+                    <ul>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                About us
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                News
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Histories of patients
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </Button>
+            <Button className="btn">
+                Services
+                <div className="dropdown_modal">
+                    <ul>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Реабілітація для дорослих
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Лікувальні блокади
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Реабілітація для дітей
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Фізеотерапія
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Консультація лікарів
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Діагностика
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </Button>
+            <Button className="btn">
+                Treatment
+                <div className="dropdown_modal">
+                    <ul>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Treatment of spine diseases
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Treatment of joint diseases
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Treatment of pain syndrome
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Children's rehabilitation
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Prevention and recovery
+                            </Link>
+                        </li>
+                        <li>
+                            <Link underline="hover" color="inherit">
+                                Treatment of diseases of the muscular system
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </Button>
             <Button className="btn">Price</Button>
             <Button className="btn">Our Team</Button>
             <Button className="btn">Contact</Button>
