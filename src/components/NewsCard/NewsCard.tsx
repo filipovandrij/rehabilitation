872dcd9535e9e42c02e449ac04f7c73553214793
Filipov <andrijfilipov73@gmail.com>
2023-03-12 14:55:59 +0200

    add redux and make likes

--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -9,9 +9,22 @@ import FavoriteIcon from '@mui/icons-material/Favorite'
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
 import './NewsCard.scss'
 import newsCardsArray from '../../utils/mocks/newsCards'
+import { useState } from 'react'
 
 type Props = {}
+
+type LikeColor = 'grey' | 'red'
 const NewsCard = (props: Props) => {
+    const [colorLike, setColorLike] = useState<LikeColor>('grey')
+
+    const isLiked = () => {
+        if (colorLike === 'grey') {
+            setColorLike('red')
+        } else {
+            setColorLike('grey')
+        }
+    }
+
     return (
         <div className="container">
             <Container className="row-container">
@@ -31,7 +44,13 @@ const NewsCard = (props: Props) => {
                                 {description}
                             </CardContent>
                             <div className="like-box">
-                                <IconButton aria-label="add to favorites">
+                                <IconButton
+                                    onClick={isLiked}
+                                    sx={{
+                                        color: colorLike,
+                                    }}
+                                    aria-label="add to favorites"
+                                >
                                     <FavoriteIcon />
                                 </IconButton>
                                 {likeCount}
