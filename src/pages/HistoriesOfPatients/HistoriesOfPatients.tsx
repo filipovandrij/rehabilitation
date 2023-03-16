7e2d7d42909afb0a8c0c57a37d6b190519a77d98
Filipov <andrijfilipov73@gmail.com>
2023-03-16 17:05:44 +0200

    make reviews with modal window

--- src/pages/HistoriesOfPatients/HistoriesOfPatients.tsx
+++ src/pages/HistoriesOfPatients/HistoriesOfPatients.tsx
@@ -1,5 +1,105 @@
+import { Card, CardContent, Container, Typography } from '@mui/material'
+import { useState } from 'react'
+import HistoryForm from '../../components/HistoryForm/HistoryForm'
+import arrReviews from '../../utils/mocks/arrReviews'
+import './HistoriesOfPatients.scss'
+
 type Props = {}
+
 const HistoriesOfPatients = (props: Props) => {
-    return <div>HistoriesOfPatients</div>
+    const [reviews, setReviews] = useState(arrReviews)
+
+    console.log(reviews)
+
+    const [newReview, setNewReview] = useState({
+        mail: '',
+        name: '',
+        text: '',
+    })
+
+    const [open, setOpen] = useState(false)
+
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const handleMail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            mail: e.target.value,
+        }))
+    }
+
+    const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            name: e.target.value,
+        }))
+    }
+
+    const handleText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            text: e.target.value,
+        }))
+    }
+
+    const onSend = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (newReview.name === '' || newReview.text === '') {
+            alert('All fields are required')
+        } else {
+            setNewReview({
+                mail: '',
+                name: '',
+                text: '',
+            })
+
+            setReviews((prevState) => {
+                return [...prevState, newReview]
+            })
+        }
+    }
+
+    return (
+        <Container maxWidth="xl">
+            <div>
+                {reviews.map((item, i) => (
+                    <Card
+                        variant="outlined"
+                        sx={{
+                            background: '#13225c',
+                            color: 'white',
+                            margin: '20px 0',
+                        }}
+                        key={i}
+                    >
+                        <CardContent>
+                            <div>
+                                {item.mail}
+                                <Typography>{item.name}:</Typography>
+                            </div>
+                            <div>{item.text}</div>
+                        </CardContent>
+                    </Card>
+                ))}
+
+                <HistoryForm
+                    open={open}
+                    onSend={onSend}
+                    newReview={newReview}
+                    handleMail={handleMail}
+                    handleName={handleName}
+                    handleText={handleText}
+                    handleClose={handleClose}
+                    handleClickOpen={handleClickOpen}
+                />
+            </div>
+        </Container>
+    )
 }
 export default HistoriesOfPatients
