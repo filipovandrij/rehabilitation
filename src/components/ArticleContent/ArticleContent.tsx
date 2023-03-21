4a1d13f6d9638cccc79b442e491e17dae314ef31
Filipov <andrijfilipov73@gmail.com>
2023-03-21 22:57:01 +0200

    add pictures to articles and add info

--- src/components/ArticleContent/ArticleContent.tsx
+++ src/components/ArticleContent/ArticleContent.tsx
@@ -3,78 +3,63 @@ import CallBack from '../MainSections/CallBack/CallBack'
 import './ArticleContent.scss'
 import InformationArticleBlock from './InformationArticleBlock/InformationArticleBlock'
 
-type Props = { title: string; description: string }
-const ArticleContent = ({ title, description }: Props) => {
+type Props = {
+    title: string
+    description: string
+    imgSrcTitle: string
+    imgSrc1: string
+    pDescription1: string
+    imgSrc2: string
+    pDescription2: string
+    imgSrc3: string
+    pDescription3: string
+}
+const ArticleContent = ({
+    title,
+    description,
+    imgSrcTitle,
+    imgSrc1,
+    pDescription1,
+    imgSrc2,
+    pDescription2,
+    imgSrc3,
+    pDescription3,
+}: Props) => {
     return (
         <>
             <Container className="main-title-container" maxWidth="xl">
                 <section className="inner-title-container">
                     <div className="title-descripton">
                         <h2>{title}</h2>
-                        <p>{title} - це визначення</p>
+                        <p>
+                            {title} - {description}
+                        </p>
                     </div>
                     <Paper
                         component="img"
-                        src="asd"
+                        src={imgSrcTitle}
                         alt="this_picture_article"
                     />
                 </section>
             </Container>
             <Container className="description-container" maxWidth="xl">
                 <div className="info-box">
-                    <img src="/images/look-this.jpg" alt="21312" />
-                    <p>
-                        {description}
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Qui quos deleniti, nisi blanditiis inventore
-                        suscipit. Fuga nobis, officiis recusandae voluptatibus
-                        vero soluta et saepe laboriosam consectetur, itaque,
-                        expedita eveniet enim.
-                    </p>
+                    <img src={imgSrc1} alt="21312" />
+                    <p>{pDescription1}</p>
                 </div>
                 <CallBack />
 
                 <div className="info-box">
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Qui quos deleniti, nisi blanditiis inventore
-                        suscipit. Fuga nobis, officiis recusandae voluptatibus
-                        vero soluta et saepe laboriosam consectetur, itaque,
-                        expedita eveniet enim.Lorem ipsum dolor sit, amet
-                        consectetur adipisicing elit. Qui quos deleniti, nisi
-                        blanditiis inventore suscipit. Fuga nobis, officiis
-                        recusandae voluptatibus vero soluta et saepe laboriosam
-                        consectetur, itaque, expedita eveniet enim.Lorem ipsum
-                        dolor sit, amet consectetur adipisicing elit. Qui quos
-                        deleniti, nisi blanditiis inventore suscipit. Fuga
-                        nobis, officiis recusandae voluptatibus vero soluta et
-                        saepe laboriosam consectetur, itaque, expedita eveniet
-                        enim.
-                    </p>
+                    <p>{pDescription2}</p>
 
-                    <img src="/images/look-this.jpg" alt="123" />
+                    <img src={imgSrc2} alt="123" />
                 </div>
             </Container>
             <InformationArticleBlock />
             <Container className="description-container" maxWidth="xl">
                 <div className="info-box">
-                    <img src="/images/look-this.jpg" alt="123" />
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Qui quos deleniti, nisi blanditiis inventore
-                        suscipit. Fuga nobis, officiis recusandae voluptatibus
-                        vero soluta et saepe laboriosam consectetur, itaque,
-                        expedita eveniet enim.Lorem ipsum dolor sit, amet
-                        consectetur adipisicing elit. Qui quos deleniti, nisi
-                        blanditiis inventore suscipit. Fuga nobis, officiis
-                        recusandae voluptatibus vero soluta et saepe laboriosam
-                        consectetur, itaque, expedita eveniet enim.Lorem ipsum
-                        dolor sit, amet consectetur adipisicing elit. Qui quos
-                        deleniti, nisi blanditiis inventore suscipit. Fuga
-                        nobis, officiis recusandae voluptatibus vero soluta et
-                        saepe laboriosam consectetur, itaque, expedita eveniet
-                        enim.
-                    </p>
+                    <img src={imgSrc3} alt="123" />
+                    <p>{pDescription3}</p>
                 </div>
             </Container>
         </>
--- src/pages/Articles/Articles.tsx
+++ src/pages/Articles/Articles.tsx
@@ -7,10 +7,31 @@ type Props = {
 const Articles = ({ cardsOfArtecles }: Props) => {
     return (
         <>
-            {article.map(({ title, description }) =>
-                cardsOfArtecles === title ? (
-                    <ArticleContent title={title} description={description} />
-                ) : null
+            {article.map(
+                ({
+                    title,
+                    description,
+                    imgSrcTitle,
+                    imgSrc1,
+                    pDescription1,
+                    imgSrc2,
+                    pDescription2,
+                    imgSrc3,
+                    pDescription3,
+                }) =>
+                    cardsOfArtecles === title ? (
+                        <ArticleContent
+                            title={title}
+                            description={description}
+                            imgSrcTitle={imgSrcTitle}
+                            imgSrc1={imgSrc1}
+                            pDescription1={pDescription1}
+                            imgSrc2={imgSrc2}
+                            pDescription2={pDescription2}
+                            imgSrc3={imgSrc3}
+                            pDescription3={pDescription3}
+                        />
+                    ) : null
             )}
         </>
     )
